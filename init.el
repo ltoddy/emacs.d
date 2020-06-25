997675f3e5cbd8d5812d35efa02ca410f97ab78b
ltoddy <taoliu0509@gmail.com>
2020-06-25 21:37:13 +0800

    set zsh as the emacs default shell

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
-(global-linum-mode 1)
-(setq linum-format "%02d|")
+(setq shell-file-name (executable-find "/bin/zsh"))
 
+(setenv "PATH" "/Users/ltoddy/.cargo/bin:/Users/ltoddy/.jenv/shims:/Users/ltoddy/.pyenv/shims:/Users/ltoddy/.pyenv/bin:/Users/ltoddy/.nvm/versions/node/v12.17.0/bin:/Users/ltoddy/.local/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/TeX/texbin:/usr/local/go/bin:/Library/Apple/usr/bin:/Applications/Racket v7.7/bin")
 
 
