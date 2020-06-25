a4c7cf118f3bfa24b7c1361730048ba6b69973dd
ltoddy <taoliu0509@gmail.com>
2020-06-25 22:38:09 +0800

    change source

--- init.el
+++ init.el
@@ -1,3 +1,6 @@
+(setq package-archives '(("gnu" . "http://elpa.emacs-china.org/gnu/")
+			 ("melpa" . "http://elpa.emacs-china.org/melpa/")))
+
 
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
